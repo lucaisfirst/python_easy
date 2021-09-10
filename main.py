e3831a51a42f2398ae0870f446eafdb175bde185
김하진 <knj9417@gmail.com>
2021-09-10 16:46:41 +0900

    2week print, input

--- main.py
+++ main.py
@@ -1,41 +1,56 @@
-# 각기 다른 변수 선언
-a = 1
-b = 3.14
-c = "Python"
+# #print 함수 연습
+# print(50)
+# print("50")
 
-# 변수에 저장된 값 출력
-print(a)
-print(b)
-print(c)
+# a = 3
+# b = 4
+# print(a, a + b)
 
-a = 3.14
-print(a)
+# # print
+# print("%d" % 50)
+# print("%f" % 3.14)
+# print("%s" % "파이썬")
+# print("%d %f" % (30, 3.14))
+# print("%d %f %s" % (30, 3.14, "파이썬"))
+# print(float(3))
 
-a = "Python"
-print(a)
+# # 실수의 소수점 자리수 출력
+# print("%f" % 3.141592)
+# print("%.2f" % 3.1415)
+# print("%08.2f" % 3.141)
 
-# 변수의 데이터 타입 출력 및 확인
-a = 1
-b = 3.14
-c = 2 + 3j
-print(type(a))
-print(type(b))
-print(type(c))
+# # format 함수를 이용한 서식 설정
+# print("%d %3d %5d" % (123, 456, 789))
+# print("{0:d} {1:3d} {2:5d}".format(123, 456, 789))
+# print("{2:d} {1:3d} {0:5d}".format(123, 456, 789))
 
-# 변수의 데이터 타입 casting
-print(float(2))
-print(str(2))
+# # 부등호를 이용한 정렬
+# print("{0:3d} {1:d}".format(3, 5))
+# print("{0:<3d} {1:d}".format(3, 5))
 
-a = 3
-print(type(a))
+# # format을 이용한 모양 만들기
+# print("{0:>8s}".format("********"))
+# print("{0:>8s}".format("*******"))
+# print("{0:>8s}".format("******"))a
+# print("{0:>8s}".format("*****"))
+# print("{0:>8s}".format("****"))
+# print("{0:>8s}".format("***"))
+# print("{0:>8s}".format("**"))
+# print("{0:>8s}".format("*"))
 
-# 사각형, 삼각형의 넓이
-wid = 3.3
-len = 7.2
+# # input 함수를 이용한 입출력
+# num1 = input("숫자1: ")
+# num2 = input("숫자2: ")
+# print(num1 + num2)
 
-area1 = wid*len
-area2 = (wid*len)/2
+# # input 함수 integer 변환
+# num1 = int(input("숫자1: "))
+# num2 = int(input("숫자2: "))
+# print(num1 + num2)
 
-print("사각형의 넓이:", area1)
-print("삼각형의 넓이", area2)
-print("사각형의 넓이: %f" % area1)
+num1 = input("숫자1: ")
+num2 = input("숫자2: ")
+
+num1 = int(num1)
+num2 = int(num2)
+print(num1 + num2)
