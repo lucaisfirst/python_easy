af8b181d2e81a388a5b74a48654832382041c9c2
김하진 <knj9417@gmail.com>
2021-09-22 20:31:27 +0900

    [Add]week3 1강

--- week3.py
+++ week3.py
@@ -23,7 +23,41 @@
 # weather = input("날씨: ")
 # print(str % (year, month, day, temp, weather))
 
-# 문자열 함수
-str = input("문자열을 입력하세요: ")
-str_len = len(str)
-print(str_len)
+# # 문자열 길이 함수
+# str = input("문자열을 입력하세요: ")
+# str_len = len(str)
+# print("입력받은 문자열은 %s이고, 길이는 %d입니다." % (str, str_len))
+
+# # 문자열 count
+# str = input("문자열을 입력하세요: ")
+# sub_str = input("확인할 문자열을 입력하세요: ")
+# count = str.count(sub_str)
+# print("%s문자열에는 %s문자열이 %d개 있습니다." % (str, sub_str, count))
+
+# # 문자열 find, index
+# str = input("문자열을 입력하세요: ")
+# sub_str = input("확인할 문자열을 입력하세요: ")
+# print("%d" % str.find(sub_str))
+# print("%d" % str.index(sub_str))
+
+# # 구분 문자열 join 연결함수
+# str = "..".join("abcd")
+# print(str)
+# str = " ".join(['aa', 'bb', 'cc'])
+# print(str)
+
+# # 구분 문자열 split 함수
+# str = "aa bb cc".split(' ')
+# print(str)
+
+# # 대문자 소문자 변경
+# str = input("문자열을 입력하시요: ")
+# print("입력문자열: %s -> 대문자로 변경: %s" % (str, str.upper()))
+# print("입력문자열: %s -> 소문자로 변경: %s" % (str, str.lower()))
+
+# # rstrip, lstrip 관련해서
+# print("abc  ".rstrip())
+# print("  abc".lstrip())
+
+# # replace 함수
+# print("ab ab abcd abc".replace("ab", "yz"))
