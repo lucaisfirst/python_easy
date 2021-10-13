7ad05873ec3598ca4eeca70a26abb688dd09f70e
김하진 <knj9417@gmail.com>
2021-10-13 23:41:44 +0900

    [Add]Week5

---
+++ week5.py
@@ -0,0 +1,8 @@
+# 기본적인 for문
+fruit = ['apple', 'banana', 'watermelon', 'strawberry']
+for i in fruit:
+    print(i)
+# 튜플을 이용한 for문
+a = [(1, 2), (3, 4), (5, 6)]
+for (first, last) in a:
+    print(first + last)
