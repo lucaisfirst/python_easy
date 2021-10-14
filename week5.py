b5bd477b631a877feb0166b3acd2e8e388ff4101
김하진 <knj9417@gmail.com>
2021-10-14 20:29:09 +0900

    dfd

--- week5.py
+++ week5.py
@@ -1,10 +1,55 @@
-# 기본적인 for문
-fruit = ['apple', 'banana', 'watermelon', 'strawberry']
-for i in fruit:
-    print(i)
+# # 기본적인 for문
+# fruit = ['apple', 'banana', 'watermelon', 'strawberry']
+# for i in fruit:
+#     print(i)
 
-# 튜플을 이용한 for문
-a = [(1, 2), (3, 4), (5, 6)]
-for (first, last) in a:
-    print(first + last)
-# 강의 이어서 듣고 예제 풀어야 한다. 오늘 안에
+# # 튜플을 이용한 for문
+# a = [(1, 2), (3, 4), (5, 6)]
+# for (first, last) in a:
+#     print(first + last)
+
+# # for문과 if문의 사용
+# scores = [100, 70, 50, 25, 85]
+
+# number = 0
+# for score in scores:
+#     number = number + 1
+#     if score >= 70:
+#         print("%d번 학생은 합격입니다." % number)
+#     else:
+#         print("%d번 학생은 불합격입니다." % number)
+
+
+# # 예제 1
+# for i in range(100):
+#     print("재미있는 파이썬!")
+# # 예제 2
+# for i in range(100):
+#     print("재미있는 파이썬!", i)
+# # 예제 3
+# for i in range(5, 11):
+#     print("재미있는 파이썬!", i)
+# # 예제 4
+# for i in range(0, 11):
+#     print("재미있는 파이썬!", i)
+# # 예제 5
+# for i in range(10, 0, -1):
+#     print("재미있는 파이썬", i)
+
+# # 예제 6
+# count = int(input("반복할 수를 입력하세요: "))
+# for i in range(count):
+#     print("재미있는 파이썬!", i)
+# # 예제 7
+# n = 5
+# factorial = 1
+# for i in range(1, n+1):
+#     factorial *= i
+# print(factorial)
+# 예제 8
+print("구구단 출력")
+for i in range(2, 10):
+    print('')
+    for j in range(1, 10):
+        print('%d * %d = %d' % (i, j, i*j))
+# 예제 9
