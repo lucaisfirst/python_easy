a373a1d286bae17c04885559b0ef50aaa00233da
김하진 <knj9417@gmail.com>
2021-10-15 16:12:13 +0900

    [Add]Week7

--- week7.py
+++ week7.py
@@ -1,11 +1,53 @@
-# 리스트 인덱스
-arr = [2, 4, 6, 8, 10]
-print(arr)
+# # 리스트 인덱스
+# arr = [2, 4, 6, 8, 10]
+# print(arr)
 
-print(arr[0])
-print(arr[1])
-print(arr[2])
-print(arr[3])
-print(arr[4])
+# print(arr[0])
+# print(arr[1])
+# print(arr[2])
+# print(arr[3])
+# print(arr[4])
 
-# 리스트 연산자
+# # 리스트 연산자
+# arr1 = [2, 4, 6, 8]
+# arr2 = [10, 12, 14, 16]
+# arr3 = arr1 + arr2
+# print(arr3)
+
+# arr1 = [2, 4, 6, 8]
+# arr2 = arr1*3
+# print(arr2)
+
+# arr1 = [2, 4, 6, 8]
+# print(4 in arr1)
+# print(10 in arr1)
+# length = len(arr1)
+# print(length)
+
+# arr1 = [2, 4, 6, 8]
+# arr1.append(10)
+# print(arr1)
+
+# arr1 = [2, 4, 6, 8]
+# arr2 = [10, 12]
+# arr1.extend(arr2)
+# print(arr1)
+
+# arr1 = [2, 4, 6, 8]
+# arr1.insert(2, 5)
+# print(arr1)
+
+# # 리스트 함수
+# arr1 = [10, 20, 30, 40, 50]
+# num = arr1.pop()
+# print(arr1)
+# print(num)
+
+# arr1 = [10, 20, 30, 42, 50]
+# num = arr1.pop(2)
+# print(arr1)
+# print(num)
+
+# arr1 = [10, 20, 30, 40, 50]
+# arr1.remove(20)
+# print(arr1)
