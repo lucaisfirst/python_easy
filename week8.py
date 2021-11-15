f8aa0b95802a8ef2fda1cd245a6c9de38f471d4a
김하진 <knj9417@gmail.com>
2021-11-15 22:20:02 +0900

    [Add]week8

--- week8.py
+++ week8.py
@@ -53,40 +53,82 @@
 #     total = 0
 #     print(list2[i])
 
-# 2차원 리스트(길이가 다른 리스트)
-# list2 = [["김하진", 6, 50, 100, 100, 80], ["허림", 5, 90, 88, 95],
-#          ["이승형", 3, 100], ["홍길동", 4, 90, 40]]
+# # 2차원 리스트(길이가 다른 리스트)
+# list2 = [["김범석", 6, 10, 20, 30, 50], ["허림", 5, 90, 90, 100],
+#          ["이승형", 3, 100], ["홍길동", 4, 90, 60]]
+
+# total = 0
+# ave = 0
 
 # for i in range(0, 4, 1):
 #     for j in range(2, list2[i][1], 1):
-#         total = total[i][j]
-#     ave = total / (list[i][1]-2)
+#         total = total + list2[i][j]
+#     ave = total / (list2[i][1]-2)
 #     list2[i].insert(2, ave)
-#     list2[i][1] = list2[i][1] + 1
+#     list2[i][1] = list2[i][1] + 1  # 변화한 길이정보 업데이트
 #     total = 0
 
-list2 = [["김하진", 6, 50, 100, 100, 80], ["허림", 5, 90, 88, 95],
-         ["이승형", 3, 100], ["홍길동", 4, 90, 40]]
+# for i in range(0, 4, 1):
+#     for j in range(0, list2[i][j], 1):
+#         if j == 0:
+#             print("%s" % list2[i][j], end="")
+#         elif j == 1:
+#             print("", end="")
+#         elif j == 2:
+#             print("%6.1f" % list2[i][j], end="")
+#         else:
+#             print("%d" % list2[i][j], end="")
+#     print("\n")
 
-total = 0
-ave = 0
+# 빙고게임
+bingoList = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [
+    11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]
+redundantList = []
+bingo = False
 
-for i in range(0, 4, 1):
-    for j in range(2, list2[i][1], 1):
-        total = total + list2[i][j]
-    ave = total / (list2[i][1]-2)
-    list2[i].insert(2, ave)
-    list2[i][1] = list2[i][1] + 1  # 변화한 길이정보 업데이트
-    total = 0
+while(bingo == False):
+    userNum = int(input())
+    if userNum < 1 or userNum > 25:
+        continue
+    elif redundantList.count(userNum) > 0:
+        continue
+    else:
+        redundantList.append(userNum)
 
-for i in range(0, 4, 1):
-    for j in range(0, list2[i][j], 1):
-        if j == 0:
-            print("%s" % list2[i][j], end="")
-        elif j == 1:
-            print("", end="")
-        elif j == 2:
-            print("%6.1f" % list2[i][j], end="")
-        else:
-            print("%3d" % list2[i][j], end="")
-    print("\n")
+        for i in range(0, 5, 1):
+            for j in range(0, 5, 1):
+                if userNum == bingoList[i][j]:
+                    bingoList[i][j] = 0
+
+        rowFlag = 0
+        colFlag = 0
+        LCrossFlag = 0
+        RCrossFlag = 0
+
+        for i in range(0, 5, 1):
+            rowFlag = 0
+            colFlag = 0
+            for j in range(0, 5, 1):
+                if bingoList[i][j] == 0:
+                    rowFlag = rowFlag + 1
+                if bingoList[j][i] == 0:
+                    colFlag = colFlag + 1
+            if rowFlag == 5 or colFlag == 5:
+                bingo = True
+                break
+
+            if bingoList[i][j] == 0:
+                LCrossFlag = LCrossFlag + 1
+            if bingoList[4 - i][j] == 0:
+                RCrossFlag = RCrossFlag + 1
+
+        if LCrossFlag == 5 or RCrossFlag == 5:
+            bingo = True
+            break
+
+    for i in range(0, 5, 1):
+        for j in range(0, 5, 1):
+            print("%3d" % bingoList[i][j], end="")
+        print("\n")
+
+print("Bingo")
