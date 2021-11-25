2fcdfbee123c86e66c44af2c4bd574328d1fa972
김하진 <knj9417@gmail.com>
2021-11-25 20:58:18 +0900

    week12

---
+++ week12.py
@@ -0,0 +1,39 @@
+# def plus3(x):
+#     y = x+3
+#     return y
+
+
+# a = 5
+# b = 3
+# b = plus3(a)
+
+# print(b)
+
+# def print3times():
+#     print("test0")
+#     print("test1")
+#     print("test2")
+
+
+# print3times()
+
+# def printXtimes(x):
+#     for i in range(0, x, 1):
+#         print("test", i)
+
+
+# a = 10
+# printXtimes(a)
+
+# def plusminus(a, b):
+#     plus = a + b
+#     minus = a - b
+#     return plus, minus
+
+
+# x = 2
+# y = 3
+# res = 0
+
+# res = plusminus(x, y)
+# print(res[0], res[1])
